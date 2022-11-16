c36b37dab26a08fc06c146eb3c7acd806fdd9137
pritam-shinde <pritamshinde.success@gmail.com>
2022-11-16 19:54:51 +0530

    banner add to faq and contact-us

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Box, Container, Grid, Typography } from '@mui/material'
+import BannerImage from '../public/Untitled-1_11zon.webp';
+import HomeCss from '../styles/Home.module.css';
 
 const FAQ = () => {
   return (
-    <div>
-      <h1>FAQ</h1>
-    </div>
+    <>
+
+    {/* Banner Section */}
+    <section>
+          <Container maxWidth="xxl" className={HomeCss.home}>
+            <Grid container >
+              <Grid item xs={12} md={10} className="mx-auto">
+                <Box py={3} className={HomeCss.bannerText} >
+                  <Grid container spacing={5}>
+                    <Grid item xs={12} md={6} className='order-1 order-md-0' >
+                      <Box className={`animate__animated animate__fadeInUp ${HomeCss.Border}`}>
+                        <Typography variant='h1'>FAQ</Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6} className='order-0 order-md-1'>
+                      <Box className='d-flex justify-content-center'>
+                        <img src={BannerImage.src} className={`${HomeCss.bannerImage} mx-auto`} />
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </section>
+    </>
   )
 }
 
